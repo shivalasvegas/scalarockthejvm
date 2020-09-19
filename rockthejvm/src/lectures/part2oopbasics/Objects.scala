5f8b727f97d452c20a19d7ddafb4600066b4cacb
shivalasvegas <shiva.king@academytrainee.com>
2020-09-19 22:41:18 +0100

    SK - Objects: added companion object and class

--- rockthejvm/src/lectures/part2oopbasics/Objects.scala
+++ rockthejvm/src/lectures/part2oopbasics/Objects.scala
@@ -1,5 +1,36 @@
 package lectures.part2oopbasics
 
 object Objects extends App{
+  //Scala does not have class level functionality ("static")
+  // Objects can have static like functionality
+  // objects can have vals vars and methods
+  object Person {
+    // an object is a singleton instance - defining type and only instance
+    // static/class - level functionality
+    val N_EYES = 2
+    def canFly: Boolean = false
+    //factory method - build Persons given some parameters
+    def from(mother: Person, father: Person): Person = new Person("Bobby")
+    def apply(mother: Person, father: Person): Person = new Person("Bobby")
+  }
+  class Person(val name: String){
+    // Instance level functionality
+  }
+  // Companions: reside in the same scope and same name
+
+  println(Person.N_EYES)
+  val mary = new Person("Mary")
+  val john = new Person("John")
+  println(mary==john) // => false
+
+  val person1 = Person
+  val person2 = Person
+  println(person1==person2)  // => true
+  // single instance that can be referered to by Person
+  val bobby1 = Person.from(mary, john)
+  val bobby2 = Person(mary, john)
+
+  // scala applications - is a scala object with
+  // def main(args: Array[String]): Unit
 
 }
